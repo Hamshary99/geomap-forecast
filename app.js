f21483f43a8923ca4ba705d7d86e4f436e26ccdf
Hamshary99 <mo.alhamshary@gmail.com>
2025-03-01 19:35:08 +0200

    Forecast v2.0

--- app.js
+++ app.js
@@ -1,31 +1,72 @@
-const forecast = require('./data_handling/forecast');
-const geocode = require('./data_handling/geocode');
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const path = require('path');
 
-//console.log(process.argv)
+app.use(express.static(path.join(__dirname, './public')));
 
-const country = process.argv[2];
+app.set('view engine', 'hbs');
 
-geocode(country, (error, data) => {
-    if(error !== undefined) {
-    console.log("ERROR: ", error);
-    } else {
-    //console.log("DATA : ", data);
-    console.log("Country: ", country);
-    console.log("Longtitude: ", data.longtitude);
-    console.log("Latitude: ", data.latitude);
-    }
+const viewsDirectory = path.join(__dirname, './template/views');
+app.set('views', viewsDirectory);
 
-    forecast(data.latitude, data.longtitude, (error, data) => {
-        if(error !== undefined) {
-        console.log("ERROR: ", error);
-        } else {
-        console.log("DATA: ", data);
-        }
-    });
+var partialsPath = path.join(__dirname, './template/partials');
+hbs.registerPartials(partialsPath);
+
+
+const port = 3000;
+
+const forecast = require('./tools/forecast');
+const geocode = require('./tools/geocode');
+
+app.get('/', (req, res) => {
+    res.render('index', {title: "Weather App"});
 });
 
-// peocess.argv vs yargs
-/*
-    yargs is a pain in the ass rn
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: "Please enter an address"
+        });
+    };
 
-*/
+    geocode(req.query.address, (error, data) => {
+        if (error !== undefined) {
+            return res.send({
+                error: error
+            });
+        }
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error !== undefined) {
+                return res.send({
+                    error: error
+                });
+            }
+            res.send({
+                location: req.query.address,
+                geomap: `latitude: ${data.latitude}, 
+                longitude: ${data.longitude}`, 
+                //forecast: forecastData,
+                temperature: `${forecastData.temperature}°C ${forecastData.emoji}`,
+                feels_like: `Feels like ${forecastData.feels_like}°C ${forecastData.emoji}`,
+                condition: forecastData.condition,
+                windSpeed: `Wind speed: ${forecastData.windSpeed} km/h`,
+                humidity: `Humidity: ${forecastData.humidity}%`
+            });
+            console.log("Location: ", req.query.address);
+            console.log("Forecast: ", forecastData);
+        });
+    });
+
+});
+
+
+// app.get('*', (req, res) => {
+//     res.send("404 Page Not Found");
+// });
+
+app.listen(port, ()=> {
+    console.log(`Server is up on port ${port}`);
+});
+
+
